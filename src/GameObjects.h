4a0bc4c8a3d7de93cd829c6058dc9171db2f3494
samCat33 <ksammy872@gmail.com>
2025-12-07 17:35:38 -0600

    Add documentation in GameObjects.h

--- src/GameObjects.h
+++ src/GameObjects.h
@@ -7,8 +7,22 @@ struct Obstacle {
     int type;
     bool active;
 
+    //description: Get x coordinate of obstacle
+    //return: int
+    //precondition: x coordinate of obstacle exist
+    //postcondition: x coordinate of obstacle is not modified
     int getX() const;
+
+    //description: Get width of obstacle
+    //return: int
+    //precondition: Width of obstacle exists
+    //postcondition: width of obstacle is not modified
     int getWidth() const;
+
+    //description: Get height of obstacle
+    //return: int
+    //precondition: Height of obstacle exists
+    //postcondition: height of obstacle is not modified
     int getHeight() const;
 };
 
@@ -19,6 +33,10 @@ struct Collectible {
     bool active;
     float animTime;
 
+    //description: Get x coordinate of collectible
+    //return: int
+    //precondition: x coordinate of collectible exists
+    //postcondition: x coordinate of obstacle is not modified
     int getX() const;
 };
 
